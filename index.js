d78c15cecf5e4c95742b3bcc95135235bb2995e1
John Weis <weis.john@gmail.com>
2015-07-23 18:49:46 -0400

    adding implementation of select false defaults

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 
 module.exports = function(schema, options) {
 
+    // default options
+    if (typeof options !== "object")
+        options = { select : false, history : false };
+
     // add the schema path
     schema.add({
         deleted: {
             type: Boolean,
-            default: false
+            default: false,
+            select: options.select
         }
     });
 
     // extend the delete function
     schema.methods['delete'] = function(fn) {
         this.deleted = true;
-        if (options && options.history && 
-            this.history && Array.isArray(this.history))
+
+        // integration with mongoose-history
+        if (options.history && Array.isArray(this.history))
             this.history.push({ status: 'deleted' });
+
         this.save(fn);
     };
 
@@ -28,6 +35,11 @@ module.exports = function(schema, options) {
 
 // middleware to faciliate soft-delete finds
 function soft_delete_middleware(next) {
-    this.find({ deleted : false });
+
+    if (typeof this._conditions === "object") {
+        if (!this._conditions.deleted) {
+            this.find({ deleted : false });
+        }
+    }
     if (typeof next === 'function') next();
 }
