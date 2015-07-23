4056149eaae40bcbc5a18dd8e851011abd766064
John Weis <weis.john@gmail.com>
2015-07-23 02:16:59 -0400

    adding middleware validator for findOne

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
-function soft_delete_middleware(next) {
-    this.find({ deleted : false });
-    if (typeof next === 'function') next();
-}
-
 module.exports = function(schema, options) {
 
     // add the schema path
@@ -17,15 +12,22 @@ module.exports = function(schema, options) {
     // extend the delete function
     schema.methods['delete'] = function(fn) {
         this.deleted = true;
-        if (options.history && this.history && Array.isArray(this.history))
+        if (options && options.history && 
+            this.history && Array.isArray(this.history))
             this.history.push({ status: 'deleted' });
         this.save(fn);
     };
 
     // allow finds to be simple
     schema.pre('find', soft_delete_middleware);
-    // schema.pre('findOne', soft_delete_middleware);
+    schema.pre('findOne', soft_delete_middleware);
     schema.pre('findOneAndUpdate', soft_delete_middleware);
     schema.pre('count', soft_delete_middleware);
 
+}
+
+// middleware to faciliate soft-delete finds
+function soft_delete_middleware(next) {
+    this.find({ deleted : false });
+    if (typeof next === 'function') next();
 }
